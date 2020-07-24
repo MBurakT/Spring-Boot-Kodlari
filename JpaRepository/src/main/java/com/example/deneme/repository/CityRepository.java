729f8d05a5c9a303fdb9110c09a7b71bd383cca5
MBurakT <150401060@ogr.comu.edu.tr>
2020-07-24 14:31:11 +0300

    Add files via upload

---
+++ JpaRepository/src/main/java/com/example/deneme/repository/CityRepository.java
@@ -0,0 +1,10 @@
+package com.example.deneme.repository;
+
+import com.example.deneme.model.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CityRepository extends JpaRepository<City, Long> {
+}
+
